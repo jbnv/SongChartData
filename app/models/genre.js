c0bd8c4936088d37321d85d77dcacf1027beabe3
Jay Bienvenu <bienvenunet@yahoo.com>
2016-02-13 16:01:33 -0600

    Implement parents.

--- app/models/genre.js
+++ app/models/genre.js
@@ -8,6 +8,13 @@ function Genre(yargs) {
   this.title = argv.t || "";
 
   this.parents = [];
+  if (argv.parent) {
+    if (argv.parent instanceof Array) {
+      this.parents = argv.parent;
+    } else {
+      this.parents.push(argv.parent);
+    }
+  }
 }
 
 Genre.prototype.typeSlug = "genre";
