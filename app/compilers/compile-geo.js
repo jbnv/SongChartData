f3f07fb712ee3b5834b009abf5bf3172d7c0f44a
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-10 20:28:37 -0600

    Add artists.

--- app/compilers/compile-geo.js
+++ app/compilers/compile-geo.js
@@ -1,9 +1,23 @@
+var fs = require("fs"),
+    meta = require('../meta');
+
 // entities: array of entities of the type
 module.exports = function(yargs,entities) {
   titles = {};
+
   entities.forEach(function(entity) {
-    titles[entity.instanceSlug] = entity.title;
+    var slug = entity.instanceSlug;
+    entity.artists = [];
+    titles[slug] = entity.title;
+
+    meta.getArtists().forEach(function(artist) {
+      if (artist.origin === slug) {
+        entity.artists.push(artist);
+      }
+    });
+
   });
+
   return {
     "all": entities,
     "titles": titles,
