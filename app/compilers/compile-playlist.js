92574dd5135b60aa779158f0dbf486f191352dbd
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-13 20:13:42 -0500

    Add title filters.

--- app/compilers/compile-playlist.js
+++ app/compilers/compile-playlist.js
@@ -1,12 +1,19 @@
-var fs = require("fs"),
+var chalk       = require("chalk"),
+    fs          = require("fs"),
+    path        = require("path"),
+    q           = require("q"),
+    util        = require("gulp-util"),
     meta = require('../meta');
 
 // entities: array of entities of the type
 module.exports = function(yargs,entities) {
+  util.log(chalk.magenta("compile-playlist.js"));
   titles = {};
 
   entities.forEach(function(entity) {
     var slug = entity.instanceSlug;
+    util.log(chalk.blue(entity.instanceSlug),entity.title);
+
     entity.songs = [];
     titles[slug] = entity.title;
 
@@ -21,6 +28,23 @@ module.exports = function(yargs,entities) {
         });
       }
     };
+
+    if (entity.filter) {
+      // Check the keys on the filter object to determine what we are doing with it.
+      // For now, assume that the object has only one key.
+      Object.keys(entity.filter).forEach(function(key) {
+        var pattern = entity.filter[key];
+        var exp = new RegExp(pattern);
+
+        if (key === "title") {
+          // Title: Argument is a pattern to match.
+          util.log("Title pattern:",chalk.magenta(pattern));
+          filter = function(song) {
+            if (exp.test(song.title)) { entity.songs.push(song); }
+          }
+        }
+      });
+    }
 
     meta.getSongs().forEach(filter);
 
