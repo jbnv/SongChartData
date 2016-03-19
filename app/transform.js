ef7018c640b118c99cbb4e70d79244fd2f834007
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-18 19:38:35 -0500

    New libraries.

---
+++ app/transform.js
@@ -0,0 +1,26 @@
+// Methods for sorting, filtering and transforming data for display.
+// This file should be the same between the data and the app.
+
+exports.sortByTitle = function(a,b) {
+  return a.title < b.title ? -1 : 1;
+}
+
+exports.sortByScore = function(a,b) {
+  return (b.score || 0) - (a.score || 0);
+}
+
+exports.sortBySongCount = function(a,b) {
+  return (b.songs.length || 0) - (a.songs.length || 0);
+}
+
+exports.sortByArtistCount = function(a,b) {
+  return (b.artists.length || 0) - (a.artists.length || 0);
+}
+
+exports.sortBySongAdjustedAverage = function(a,b) {
+  return (b.songAdjustedAverage || 0) - (a.songAdjustedAverage || 0);
+}
+
+exports.sortByArtistAdjustedAverage = function(a,b) {
+  return (b.artistAdjustedAverage || 0) - (a.artistAdjustedAverage || 0);
+}
