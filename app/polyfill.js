98c37f72809ddf7ad8fe70dafbca02ed79a279d7
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-23 20:18:22 -0500

    Add find() and findBySlug().

--- app/polyfill.js
+++ app/polyfill.js
@@ -57,6 +57,33 @@ Array.prototype.expand = function(all,transform) {
 
 };
 
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
+Array.prototype.findBySlug = function(slug) {
+  return this.find(function(el) { return el.instanceSlug === slug; })
+}
+
 if (!String.prototype.startsWith) {
     String.prototype.startsWith = function(searchString, position){
       position = position || 0;
