80aec325fad4b68e15344f29f9a380363a7618b6
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-25 21:46:48 -0500

    Add titles and scores routes.

--- server/entity.js
+++ server/entity.js
@@ -1,4 +1,5 @@
-var meta = require("../app/meta");
+var meta = require("../app/meta"),
+    scoring = require("../app/scoring");
 
 module.exports = function(parameters,app) {
 
@@ -10,6 +11,34 @@ module.exports = function(parameters,app) {
     console.log("GET /"+pluralSlug);
     content = meta.getCompiledCollection(storageSlug)();
     res.send(content);
+  });
+
+  app.get("/"+pluralSlug+"/titles", function(req, res) {
+    console.log("GET /"+pluralSlug+"/titles");
+    content = meta.getCompiledCollection(storageSlug)();
+    var outbound = {};
+    content.forEach(function(item) { outbound[item.instanceSlug] = item.title; });
+    res.send(outbound);
+  });
+
+  app.get("/"+pluralSlug+"/scores", function(req, res) {
+    console.log("GET /"+pluralSlug+"/scores");
+    content = meta.getCompiledCollection(storageSlug)();
+    res.send(content.map(function(item) {
+      var outbound = {};
+      outbound.slug = item.instanceSlug;
+      outbound.title = item.title;
+      outbound.score = item.score;
+      if (item.songs) {
+        outbound.songCount = item.songs.length;
+        outbound.songAdjustedAverage = scoring.adjustedAverage(item.score,item.songs.length);
+      }
+      if (item.artists) {
+        outbound.artistCount = item.artists.length;
+        outbound.artistAdjustedAverage = scoring.adjustedAverage(item.score,item.artists.length);
+      }
+      return outbound;
+    }));
   });
 
   var itemExpression = "^\\/"+singularSlug+"\\/(.+)$";
