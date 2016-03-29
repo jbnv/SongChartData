370ec91ae95438c56f951495a1ebaf10c12245fe
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-28 21:16:38 -0500

    Refactor common functions to library.

--- server/entity.js
+++ server/entity.js
@@ -1,54 +1,23 @@
-var meta = require("../app/meta"),
+var functions = require("./functions"),
+    meta = require("../app/meta"),
     scoring = require("../app/scoring");
 
 module.exports = function(parameters,app) {
 
-  var singularSlug = parameters.singular || parameters;
-  var pluralSlug = parameters.plural || singularSlug+"s";
-  var storageSlug = parameters.storage || singularSlug;
+  var slugs = {};
+  slugs.singularSlug = parameters.singular || parameters;
+  slugs.pluralSlug = parameters.plural || slugs.singularSlug+"s";
+  slugs.storageSlug = parameters.storage || slugs.singularSlug;
 
-  app.get("/"+pluralSlug, function(req, res) {
-    console.log("GET /"+pluralSlug);
-    content = meta.getCompiledCollection(storageSlug)();
-    res.send(content);
-  });
+  app.get("/"+slugs.pluralSlug, functions.getSome(slugs));
 
-  app.get("/"+pluralSlug+"/titles", function(req, res) {
-    console.log("GET /"+pluralSlug+"/titles");
-    content = meta.getCompiledCollection(storageSlug)();
-    var outbound = {};
-    content.forEach(function(item) { outbound[item.instanceSlug] = item.title; });
-    res.send(outbound);
-  });
+  app.get("/"+slugs.pluralSlug+"/titles", functions.getTitles(slugs));
 
-  app.get("/"+pluralSlug+"/scores", function(req, res) {
-    console.log("GET /"+pluralSlug+"/scores");
-    content = meta.getCompiledCollection(storageSlug)();
-    res.send(content.map(function(item) {
-      var outbound = {};
-      outbound.slug = item.instanceSlug;
-      outbound.title = item.title;
-      outbound.score = item.score;
-      if (item.songs) {
-        outbound.songCount = item.songs.length;
-        outbound.songAdjustedAverage = scoring.adjustedAverage(item.score,item.songs.length);
-      }
-      if (item.artists) {
-        outbound.artistCount = item.artists.length;
-        outbound.artistAdjustedAverage = scoring.adjustedAverage(item.score,item.artists.length);
-      }
-      return outbound;
-    }));
-  });
+  app.get("/"+slugs.pluralSlug+"/scores", functions.getScores(slugs));
 
-  var itemExpression = "^\\/"+singularSlug+"\\/(.+)$";
+  var itemExpression = "^\\/"+slugs.singularSlug+"\\/(.+)$";
 
-  app.get(new RegExp(itemExpression), function(req, res) {
-    console.log("GET /"+singularSlug,req.params[0]);
-    var itemSlug = req.params[0];
-    content = meta.getCompiledObject(storageSlug,itemSlug)();
-    res.send(content);
-  });
+  app.get(new RegExp(itemExpression), functions.getOne(slugs));
 
   //TODO PUT route - create item
 
---
+++ server/functions.js
@@ -0,0 +1,65 @@
+var meta = require("../app/meta"),
+    scoring = require("../app/scoring");
+
+exports.getOne = function(slugs,filterFn) {
+
+  return function(req, res) {
+    console.log("GET /"+slugs.singularSlug,req.params[0]);
+    var itemSlug = req.params[0];
+    content = meta.getCompiledObject(slugs.storageSlug,itemSlug)();
+    if (filterFn) {
+      content = content.filter(filterFn);
+    }
+    res.send(content);
+  };
+
+}
+
+exports.getSome = function(slugs,filterFn) {
+
+  return function(req, res) {
+    console.log("GET /"+slugs.pluralSlug);
+    content = meta.getCompiledCollection(slugs.storageSlug)();
+    if (filterFn) {
+      content = content.filter(filterFn);
+    }
+    res.send(content);
+  };
+
+}
+
+exports.getTitles = function(slugs) {
+
+  return function(req, res) {
+    console.log("GET /"+slugs.pluralSlug+"/titles");
+    content = meta.getCompiledCollection(slugs.storageSlug)();
+    var outbound = {};
+    content.forEach(function(item) { outbound[item.instanceSlug] = item.title; });
+    res.send(outbound);
+  };
+
+}
+
+exports.getScores = function(slugs) {
+
+  return function(req, res) {
+    console.log("GET /"+slugs.pluralSlug+"/scores");
+    content = meta.getCompiledCollection(slugs.storageSlug)();
+    res.send(content.map(function(item) {
+      var outbound = {};
+      outbound.slug = item.instanceSlug;
+      outbound.title = item.title;
+      outbound.score = item.score;
+      if (item.songs) {
+        outbound.songCount = item.songs.length;
+        outbound.songAdjustedAverage = scoring.adjustedAverage(item.score,item.songs.length);
+      }
+      if (item.artists) {
+        outbound.artistCount = item.artists.length;
+        outbound.artistAdjustedAverage = scoring.adjustedAverage(item.score,item.artists.length);
+      }
+      return outbound;
+    }));
+  };
+
+}
